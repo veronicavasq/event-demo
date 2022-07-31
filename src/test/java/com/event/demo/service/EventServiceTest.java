e27553d92c8604b2f867763bbb9131bd3f0d68c6
Veronica Vasquez <veronicavasquez.92@gmail.com>
2022-08-01 00:04:52 +0200

    Add test case with multithreading

--- src/test/java/com/event/demo/service/EventServiceTest.java
+++ src/test/java/com/event/demo/service/EventServiceTest.java
@@ -13,8 +13,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.lang.Thread.currentThread;
 
 public class EventServiceTest {
 
@@ -22,25 +28,25 @@ public class EventServiceTest {
 
     @BeforeEach
     void init() {
-        eventService = new EventService();
+        this.eventService = new EventService();
     }
 
     @Test
     public void processEventInvalidEventTest() {
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(null));
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(new Event()));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(null));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(new Event()));
 
         Event invalidEvent = new Event();
         invalidEvent.setId(UUID.randomUUID().toString());
         invalidEvent.setLicensePlate(RandomStringUtils.randomAlphabetic(10));
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(invalidEvent));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(invalidEvent));
     }
 
     @Test
     public void processEventInvalidSpeedEventTest() {
         Event event = EventTestUtil.buildEvent(EventType.SPEED);
         event.setLimit(null);
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(event));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(event));
     }
 
     @Test
@@ -49,40 +55,40 @@ public class EventServiceTest {
         Event event = EventTestUtil.buildEvent(EventType.SPEED);
         event.setSpeed(10.0F);
         event.setLimit(100.0F);
-        Violation violation = eventService.processEvent(event);
+        Violation violation = this.eventService.processEvent(event);
         Assertions.assertNull(violation);
 
         // Send same speed even with no violation
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(event));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(event));
 
         // Speed violation
         Event violationEvent = EventTestUtil.buildEvent(EventType.SPEED);
         violationEvent.setSpeed(100.0F);
         violationEvent.setLimit(15.0F);
-        violation = eventService.processEvent(violationEvent);
+        violation = this.eventService.processEvent(violationEvent);
         EventTestUtil.validateViolation(violation, violationEvent.getId());
 
         // Send same speed even with no violation
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(violationEvent));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(violationEvent));
     }
 
     @Test
     public void processEventRedLightTest() throws EventException {
         // No red light violation
         Event event = EventTestUtil.buildEvent(EventType.SPEED);
-        Violation violation = eventService.processEvent(event);
+        Violation violation = this.eventService.processEvent(event);
         Assertions.assertNull(violation);
 
         // Send same speed even with no violation
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(event));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(event));
 
         // Speed violation
         Event violationEvent = EventTestUtil.buildEvent(EventType.RED_LIGHT);
-        violation = eventService.processEvent(violationEvent);
+        violation = this.eventService.processEvent(violationEvent);
         EventTestUtil.validateViolation(violation, violationEvent.getId());
 
         // Send same speed even with no violation
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.processEvent(violationEvent));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.processEvent(violationEvent));
     }
 
     @Test
@@ -95,58 +101,58 @@ public class EventServiceTest {
         Event redLightEventCustomLicense = EventTestUtil.buildEvent(EventType.RED_LIGHT);
         redLightEventCustomLicense.setLicensePlate("custom-license");
 
-        Violation speedViolation = eventService.processEvent(speedEvent);
+        Violation speedViolation = this.eventService.processEvent(speedEvent);
         EventTestUtil.validateViolation(speedViolation, speedEvent.getId());
         Assertions.assertNotNull(speedViolation.getId());
 
-        Violation redLightViolation = eventService.processEvent(redLightEvent);
+        Violation redLightViolation = this.eventService.processEvent(redLightEvent);
         EventTestUtil.validateViolation(redLightViolation, redLightEvent.getId());
         Assertions.assertNotNull(redLightViolation.getId());
 
-        Violation redLightViolationCustomLicense = eventService.processEvent(redLightEventCustomLicense);
+        Violation redLightViolationCustomLicense = this.eventService.processEvent(redLightEventCustomLicense);
         EventTestUtil.validateViolation(redLightViolationCustomLicense, redLightEventCustomLicense.getId());
         Assertions.assertNotNull(redLightViolationCustomLicense.getId());
 
         // Get all violations
-        List<Violation> violations = eventService.getViolations(null, null, null);
+        List<Violation> violations = this.eventService.getViolations(null, null, null);
         Assertions.assertNotNull(violations);
         Assertions.assertEquals(violations.size(), 3);
 
         // Only red light violations
-        List<Violation> redLightViolations = eventService.getViolations(EventType.RED_LIGHT, null, null);
+        List<Violation> redLightViolations = this.eventService.getViolations(EventType.RED_LIGHT, null, null);
         Assertions.assertNotNull(redLightViolations);
         Assertions.assertEquals(redLightViolations.size(), 2);
 
         // Only speed violations
-        List<Violation> speedViolations = eventService.getViolations(EventType.SPEED, null, null);
+        List<Violation> speedViolations = this.eventService.getViolations(EventType.SPEED, null, null);
         Assertions.assertNotNull(speedViolations);
         Assertions.assertEquals(speedViolations.size(), 1);
 
         // Only custom license violations
-        List<Violation> customLicenseViolations = eventService.getViolations(null, null, "custom-license");
+        List<Violation> customLicenseViolations = this.eventService.getViolations(null, null, "custom-license");
         Assertions.assertNotNull(customLicenseViolations);
         Assertions.assertEquals(customLicenseViolations.size(), 1);
 
         // Only paid violations
-        eventService.violationPayment(speedViolation.getId());
-        eventService.violationPayment(redLightViolation.getId());
-        List<Violation> paidViolations = eventService.getViolations(null, true, null);
+        this.eventService.violationPayment(speedViolation.getId());
+        this.eventService.violationPayment(redLightViolation.getId());
+        List<Violation> paidViolations = this.eventService.getViolations(null, true, null);
         Assertions.assertNotNull(paidViolations);
         Assertions.assertEquals(paidViolations.size(), 2);
     }
 
     @Test
     public void violationPaymentTest() throws EventException {
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.violationPayment(null));
-        Assertions.assertThrows(InvalidEventException.class, () -> eventService.violationPayment("test-id"));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.violationPayment(null));
+        Assertions.assertThrows(InvalidEventException.class, () -> this.eventService.violationPayment("test-id"));
 
         Event event = EventTestUtil.buildEvent(EventType.RED_LIGHT);
-        Violation violation = eventService.processEvent(event);
+        Violation violation = this.eventService.processEvent(event);
 
-        eventService.violationPayment(violation.getId());
-        Assertions.assertThrows(ViolationPaidException.class, () -> eventService.violationPayment(violation.getId()));
+        this.eventService.violationPayment(violation.getId());
+        Assertions.assertThrows(ViolationPaidException.class, () -> this.eventService.violationPayment(violation.getId()));
 
-        List<Violation> paidViolations = eventService.getViolations(null, true, null);
+        List<Violation> paidViolations = this.eventService.getViolations(null, true, null);
         Assertions.assertNotNull(paidViolations);
         Assertions.assertEquals(paidViolations.size(), 1);
         Assertions.assertEquals(paidViolations.get(0).getId(), violation.getId());
@@ -159,28 +165,105 @@ public class EventServiceTest {
         Event event = EventTestUtil.buildEvent(EventType.SPEED);
         event.setSpeed(100.0F);
         event.setLimit(15.0F);
-        eventService.processEvent(event);
+        this.eventService.processEvent(event);
 
         // No violation event
         event = EventTestUtil.buildEvent(EventType.SPEED);
         event.setSpeed(.0F);
         event.setLimit(15.0F);
-        eventService.processEvent(event);
+        this.eventService.processEvent(event);
 
         // red light events
         event = EventTestUtil.buildEvent(EventType.RED_LIGHT);
-        eventService.processEvent(event);
+        this.eventService.processEvent(event);
 
         event = EventTestUtil.buildEvent(EventType.RED_LIGHT);
-        Violation violation = eventService.processEvent(event);
+        Violation violation = this.eventService.processEvent(event);
 
         // Pay one violation event
-        eventService.violationPayment(violation.getId());
+        this.eventService.violationPayment(violation.getId());
 
         // Get summary
-        ViolationSummary summary = eventService.getViolationSummary();
+        ViolationSummary summary = this.eventService.getViolationSummary();
         Assertions.assertEquals(summary.getTotalPaid(), 1);
         Assertions.assertEquals(summary.getTotalUnpaid(), 2);
+    }
+
+    @Test
+    public void testProcessDifferentEventWithConcurrency() {
+
+        Event speedEvent = EventTestUtil.buildEvent(EventType.SPEED);
+        speedEvent.setSpeed(120.0F);
+        speedEvent.setLimit(100.0F);
+
+        Event redLightEvent = EventTestUtil.buildEvent(EventType.RED_LIGHT);
+
+        ExecutorService exec = Executors.newScheduledThreadPool(3);
+
+        CompletableFuture<Violation> event1 = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("Event:" + currentThread().getId() + " - time:" + System.currentTimeMillis());
+                    try {
+                        return this.eventService.processEvent(speedEvent);
+                    } catch (EventException exception) {
+                        return null;
+                    }
+                }, exec);
+
+        CompletableFuture<Violation> event2 = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("Event:" + currentThread().getId() + " - time:" + System.currentTimeMillis());
+                    try {
+                        return this.eventService.processEvent(redLightEvent);
+                    } catch (EventException exception) {
+                        return null;
+                    }
+                }, exec);
+
+        CompletableFuture<Violation> event3 = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("Event:" + currentThread().getId() + " - time:" + System.currentTimeMillis());
+                    try {
+                        return this.eventService.processEvent(speedEvent);
+                    } catch (EventException exception) {
+                        return null;
+                    }
+                }, exec);
+
+        CompletableFuture.allOf(event1, event2, event3).join();
+
+        List<Violation> violations = this.eventService.getViolations(null, null, null);
+        Assertions.assertNotNull(violations);
+        Assertions.assertEquals(violations.size(), 2);
+
+    }
+
+    @Test
+    public void testProcessOneEventWithConcurrency() {
+        Event event = EventTestUtil.buildEvent(EventType.SPEED);
+        event.setSpeed(120.0F);
+        event.setLimit(100.0F);
+
+        ExecutorService exec = Executors.newScheduledThreadPool(100);
+
+        List<CompletableFuture<Violation>> futures = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            futures.add(
+                    CompletableFuture.supplyAsync(() -> {
+                        System.out.println("Event:" + currentThread().getId() + " - time:" + System.currentTimeMillis());
+                        try {
+                            return this.eventService.processEvent(event);
+                        } catch (EventException exception) {
+                            return null;
+                        }
+                    }, exec));
+        }
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        List<Violation> violations = this.eventService.getViolations(null, null, null);
+        Assertions.assertNotNull(violations);
+        Assertions.assertEquals(violations.size(), 1);
+
     }
 
 }
